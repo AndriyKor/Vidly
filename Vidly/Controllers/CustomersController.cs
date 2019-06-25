817095e54fdbdd33ecaf1a93422415fd72e8935d
Andriy Korovetskyy <andriy.korovetskyy@gmail.com>
2019-06-25 18:57:11 +0300

    Display the list of Customers and Movies

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> customers = new List<Customer>
+        {
+            new Customer { Name = "David de Gea", Id = 1 },
+            new Customer { Name = "Mario Balotelli", Id = 2 }
+        };
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customersViewModel = new CustomersViewModel
+            {
+                Customers = customers
+            };
+
+            return View(customersViewModel);
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var customer = customers.FirstOrDefault(c => c.Id == Id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        List<Movie> movies = new List<Movie>
+        {
+            
+            new Movie { Name = "Iron Man", Id = 1 },
+            new Movie { Name = "X-Man", Id = 2 },
+            new Movie { Name = "Transformers", Id = 3 }
+            
+        };
+
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            return Content("id = " + Id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            var moviesViewModel = new MoviesViewModel
+            {
+                Movies = movies
+            };
+
+            return View(moviesViewModel);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int? year, int? month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
