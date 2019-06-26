bf5e449459bcee742749570581877a610884e8b6
Andriy Korovetskyy <andriy.korovetskyy@gmail.com>
2019-06-26 22:34:42 +0300

    Session 4; Before Exersice

--- Vidly/Models/ApplicationDBContext.cs
+++ Vidly/Models/ApplicationDBContext.cs
@@ -10,5 +10,6 @@
 
             public DbSet<Customer> Customers { get; set; } // My domain models
             public DbSet<Movie> Movies { get; set; } // My domain models
+            public DbSet<MembershipType> MembershipTypes { get; set; }
         }
     }
